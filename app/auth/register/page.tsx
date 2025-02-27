07be7c56c53c012e53c42008f0d1864d316dd2ac
Anil Ozsoy <anilozsoy@gmail.com>
2025-02-27 15:50:21 +0100

    feat(auth): add registration page component for user registration feat(home): update home page to include link to the new registration page

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+export default function RegisterPage() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="w-1/2 h-1/2 bg-white rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold">Register</h1>
+                <form>
+     
+                </form>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex-col w-screen h-screen">
       <h1>Home</h1>
-      <Link href="/about">About</Link>
+      <Link href="/auth/register">Register</Link>
     </div>
   );
 }
