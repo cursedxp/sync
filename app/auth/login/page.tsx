abd112eabcdaf31cb541adc0ac0ddbd175cfb62c
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-07 10:03:16 +0100

    feat(auth): add login page with form validation and authentication logic refactor(auth/register): simplify register page layout and improve form structure for better readability and maintainability

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import TextField from "@/app/components/common/TextField/TextField";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold my-4">Login</h1>
+      <div className="mb-4">
+        <p>
+          Don&apos;t have an account?
+          <Link href="/auth/register" className="font-semibold underline">
+            Register
+          </Link>
+        </p>
+      </div>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Email is required"),
+          password: Yup.string().required("Required"),
+        })}
+        onSubmit={async (values) => {
+          const result = await signIn("credentials", {
+            ...values,
+            redirect: false,
+          });
+
+          if (result?.error) {
+            console.error(result.error);
+          } else {
+            router.push("/client");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <TextField
+              label="Email address"
+              name="email"
+              type="email"
+              placeholder="you@example.com"
+            />
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              placeholder=""
+            />
+            <Link
+              href="/auth/forgot-password"
+              className="underline font-medium mb-4 block"
+            >
+              Forgot your password?
+            </Link>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full py-3.5 px-4 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-600 transition-colors duration-300"
+            >
+              {isSubmitting ? "Logging in..." : "Login"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -7,7 +7,6 @@ import TextField from "@/app/components/common/TextField/TextField";
 import CheckBox from "@/app/components/common/CheckBox/CheckBox";
 import Select from "@/app/components/common/Select/Select";
 
-
 export default function RegisterPage() {
   const countries = [
     { value: "at", label: "Austria" },
@@ -48,99 +47,97 @@ export default function RegisterPage() {
   ];
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="max-w-xl w-full">
-        <h1 className="text-4xl font-bold my-4">Register</h1>
-        <div className="mb-4">
-          <p>
-            Already have an account?{" "}
-            <Link href="/auth/login" className="font-semibold underline">
-              Log in
-            </Link>
-          </p>
-        </div>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            terms: false,
-            news: false,
-            country: "at",
-          }}
-          validationSchema={Yup.object({
-            email: Yup.string()
-              .email("Invalid email address")
-              .required("Email is required"),
-            password: Yup.string()
-              .min(8, "Choose a password with at least 8 characters.")
-              .required("Password is required"),
-            terms: Yup.boolean()
-              .oneOf(
-                [true],
-                "Accept the Terms and Conditions and Privacy Policy to continue"
-              )
-              .required(
-                "Accept the Terms and Conditions and Privacy Policy to continue"
-              ),
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            console.log(values);
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting, values, setFieldValue }) => (
-            <Form>
-              <TextField
-                label="Email address"
-                name="email"
-                type="email"
-                placeholder="you@example.com"
-              />
-              <TextField
-                label="Password"
-                name="password"
-                type="password"
-                placeholder=""
-              />
-              <PasswordStrengthMeter password={values.password} />
-              <Select
-                name="country"
-                label="Country of your business"
-                options={countries}
-                value={values.country}
-                onChange={(value) => setFieldValue("country", value)}
-                showFlag={true}
-              />
-              <CheckBox name="terms">
-                <p>
-                  By ticking this box you accept the{" "}
-                  <Link href="/terms" className="underline">
-                    Terms and Conditions
-                  </Link>{" "}
-                  and acknowledge that you have read and understood the{" "}
-                  <Link href="/privacy" className="underline">
-                    Privacy Policy
-                  </Link>
-                </p>
-              </CheckBox>
-              <CheckBox name="news">
-                <p>
-                  (Optional) We will occasionally contact you with our latest
-                  news and offers. You can unsubscribe at any time. By ticking
-                  this box you indicate that you do not want to be contacted.
-                </p>
-              </CheckBox>
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="w-full py-3 px-4 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-600 transition-colors duration-300"
-              >
-                {isSubmitting ? "Submitting..." : "Next"}
-              </button>
-            </Form>
-          )}
-        </Formik>
+    <>
+      <h1 className="text-4xl font-bold my-4">Register</h1>
+      <div className="mb-4">
+        <p>
+          Already have an account?{" "}
+          <Link href="/auth/login" className="font-semibold underline">
+            Log in
+          </Link>
+        </p>
       </div>
-    </div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          terms: false,
+          news: false,
+          country: "at",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Email is required"),
+          password: Yup.string()
+            .min(8, "Choose a password with at least 8 characters.")
+            .required("Password is required"),
+          terms: Yup.boolean()
+            .oneOf(
+              [true],
+              "Accept the Terms and Conditions and Privacy Policy to continue"
+            )
+            .required(
+              "Accept the Terms and Conditions and Privacy Policy to continue"
+            ),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, values, setFieldValue }) => (
+          <Form>
+            <TextField
+              label="Email address"
+              name="email"
+              type="email"
+              placeholder="you@example.com"
+            />
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              placeholder=""
+            />
+            <PasswordStrengthMeter password={values.password} />
+            <Select
+              name="country"
+              label="Country of your business"
+              options={countries}
+              value={values.country}
+              onChange={(value) => setFieldValue("country", value)}
+              showFlag={true}
+            />
+            <CheckBox name="terms">
+              <p>
+                By ticking this box you accept the{" "}
+                <Link href="/terms" className="underline">
+                  Terms and Conditions
+                </Link>{" "}
+                and acknowledge that you have read and understood the{" "}
+                <Link href="/privacy" className="underline">
+                  Privacy Policy
+                </Link>
+              </p>
+            </CheckBox>
+            <CheckBox name="news">
+              <p>
+                (Optional) We will occasionally contact you with our latest news
+                and offers. You can unsubscribe at any time. By ticking this box
+                you indicate that you do not want to be contacted.
+              </p>
+            </CheckBox>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full py-3.5 px-4 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-600 transition-colors duration-300"
+            >
+              {isSubmitting ? "Submitting..." : "Next"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 }
