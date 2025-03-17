39e2faea846633d5c1c0fcb7a32c46714d7e3385
Anil Ozsoy <anilozsoy@gmail.com>
2025-03-17 15:23:04 +0100

    feat(auth/layout.tsx): enhance layout with dynamic background images based on route for improved user experience feat(auth/layout.tsx): add new images for login and signup backgrounds to enrich the visual design

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,28 @@
+"use client";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="max-w-xl w-full">{children}</div>
+    <div className="w-screen h-screen flex items-center">
+      <div className="relative h-full w-1/2">
+        <Image
+          src={
+            pathname === "/auth/login"
+              ? "/images/auth/cayley-nossiter-LO3IuL-wgME-unsplash.jpg"
+              : "/images/auth/toa-heftiba-l_ExpFwwOEg-unsplash.jpg"
+          }
+          alt="logo"
+          fill
+          className="w-full h-full object-cover filter brightness-145"
+          priority
+        />
+      </div>
+      <div className="w-1/2 p-40">{children}</div>
     </div>
   );
 }
